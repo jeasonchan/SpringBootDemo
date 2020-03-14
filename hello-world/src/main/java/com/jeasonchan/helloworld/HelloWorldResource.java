6ad1ca147d8a2cd1755b431d4babf5ab4c1bdd37
jeasonchan <439994526@qq.com>
2020-03-15 01:03:06 +0800

    天实现helloworld，僚；包含，球路径参数、查询参数、请求体注释

--- hello-world/src/main/java/com/jeasonchan/helloworld/HelloWorldResource.java
+++ hello-world/src/main/java/com/jeasonchan/helloworld/HelloWorldResource.java
@@ -1,4 +1,44 @@
 package com.jeasonchan.helloworld;
 
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController  //表明是rest接口，而不是返回一个页面
 public class HelloWorldResource {
+
+
+    //可以直接使用getmapping进行代替，更加简洁
+    @RequestMapping(
+            path = "/hello/{name}",
+            method = RequestMethod.GET
+    )
+    public Map<String, Object> sayHello(@PathVariable("name") String name,
+                                        @RequestParam(required = false) int age,
+                                        @RequestBody List<String> hobbies,
+                                        @RequestHeader Map<String, Object> header
+    ) {
+        Map<String, Object> result = new HashMap<>();
+        result.put("name", name);
+        result.put("age", age);
+        result.put("hobbies", hobbies);
+        result.put("header", header);
+
+        return result;
+    }
+
+
+
+    /*
+    springboot中，基本的参数类型有三种：
+    路径参数，使用 PathVariable  注解，和javax中的pathParam不兼容！
+    查询参数，使用 RequestParam   注解，和javax中的queryParam不兼容
+    请求体，使用  RequestBody  注解，和javax中的body不兼容
+    请求头，使用 RequestHeader 注解，包含客户端类型、token等参数，使用的方式有多种
+
+
+     */
 }
