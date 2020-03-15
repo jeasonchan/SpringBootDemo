d5775fae25f8181626f26df4c7a157fff39332a2
jeasonchan <439994526@qq.com>
2020-03-16 00:08:08 +0800

    add comments

--- read-properties/src/main/java/com/jeasonchan/readproperties/controller/ReadPropertyController.java
+++ read-properties/src/main/java/com/jeasonchan/readproperties/controller/ReadPropertyController.java
@@ -2,7 +2,6 @@ package com.jeasonchan.readproperties.controller;
 
 import com.jeasonchan.readproperties.property.ApplicationProperty;
 import com.jeasonchan.readproperties.property.DevelopProperty;
-import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +31,7 @@ public class ReadPropertyController {
 
 
     /*
-
+    要点
     ====================
     使用了两种不同的方式，从配置文件中读取配置：
     @value
@@ -40,11 +39,24 @@ public class ReadPropertyController {
     和
 
     @component + @configurationProperty
+
     ======================
     在构造函数中使用autowerid自动注入组件实例
 
 
-    =========
+    ======================
+    配置文件里的默认配置属性，也就是隐藏的配置类，包括但不限于：
+    server:
+         port: 8080
+         servlet:
+                context-path: /demo
+
+    spring:
+      profiles:
+        active: B
+
+    application.yml和其余的A配置和B配置形成互补配置，
+    因此，application和developer两个配置实例的数据也可能拿到
 
 
 
