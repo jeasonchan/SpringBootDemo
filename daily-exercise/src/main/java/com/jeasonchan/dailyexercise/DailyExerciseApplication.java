3f3717f59b7d8bcb6ffd32bf99f63e88048e2e24
jeasonchan <439994526@qq.com>
2020-04-22 21:54:30 +0800

    增加日常练习模块

---
+++ daily-exercise/src/main/java/com/jeasonchan/dailyexercise/DailyExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.jeasonchan.dailyexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyExerciseApplication.class, args);
+    }
+
+}
