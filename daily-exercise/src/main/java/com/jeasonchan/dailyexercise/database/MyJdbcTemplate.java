087821712bbd023d89a5beaf4f35aa89f2ea5099
jeasonchan <439994526@qq.com>
2020-04-24 23:49:11 +0800

    增加数据库多源配置模块

--- daily-exercise/src/main/java/com/jeasonchan/dailyexercise/database/MyJdbcTemplate.java
+++ daily-exercise/src/main/java/com/jeasonchan/dailyexercise/database/MyJdbcTemplate.java
@@ -1,6 +1,7 @@
 package com.jeasonchan.dailyexercise.database;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
@@ -11,5 +12,14 @@ public class MyJdbcTemplate {
     @Autowired
     public MyJdbcTemplate(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
+        System.out.println("jdbcTemplate通过构造函数注入完成！");
+    }
+
+
+    @Override
+    public String toString() {
+        return "MyJdbcTemplate{" +
+                "jdbcTemplate=" + jdbcTemplate +
+                '}';
     }
 }
