4f5d0c40f5ea26797b34ebeef6b931155065f20f
jeasonchan <439994526@qq.com>
2020-05-12 00:25:01 +0800

    将并查集和restful接口相结合

--- daily-exercise/src/main/java/com/jeasonchan/dailyexercise/leetcode/并查集专项练习/奇偶数分类/Controller.java
+++ daily-exercise/src/main/java/com/jeasonchan/dailyexercise/leetcode/并查集专项练习/奇偶数分类/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 @RestController
 public class Controller {
@@ -14,11 +15,40 @@ public class Controller {
     public Map<String, Object> solve(@PathVariable("implType") String implType, @RequestBody List<Integer> values) {
 
         UnionFindSet unionFindSet = new UnionFindSet(values);
+        ConcurrentLinkedQueue<Integer> rootValues = new ConcurrentLinkedQueue<>();
+
+        for (Integer value : values) {
+            if (rootValues.isEmpty()) {
+                rootValues.add(value);
+            } else {
+                for (Integer eachRoot : rootValues) {
+                    int currentValueRoot = unionFindSet.findRootValue(value);
+                    int mergeResult = unionFindSet.compareThenMerge(eachRoot, currentValueRoot);
+
+                    if (UnionFindSet.CAN_NOT_MERGE == mergeResult) {
+                        rootValues.add(currentValueRoot);
+                        continue;
+                    }
+
+                    if (currentValueRoot == mergeResult) {
+                        rootValues.remove(eachRoot);
+                        rootValues.add(currentValueRoot);
+                    }
+
+
+                }
+
+
+            }
+
+
+        }
 
 
         Map<String, Object> result = new HashMap<>();
         result.put("implType", implType);
         result.put("unionFindSet", unionFindSet.getValue2RootArray());
+        result.put("rootValues", rootValues);
         return result;
     }
 }
